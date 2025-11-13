5a72438a0089ded74a2432e9c9b88f18253c76d0
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2025-11-13 15:03:45 -0300

    ADD - enhance movie card layout and search functionality

--- js/format.js
+++ js/format.js
@@ -1,10 +1,9 @@
-
 export const Format = {
   search(search) {
     if (typeof search === "string") {
-      const newSearch = search.trim().replace(" ", "+");
+      const newSearch = search.trim().replace(/\s+/g, "+");
       return newSearch;
-    };
-    return;
+    }
+    return "";
   }
-}
+};
