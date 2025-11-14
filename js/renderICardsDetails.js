0113df4a092ed9d9ee4ffbdbd6d719a036d179dc
Paulo Henrique Alves Vieira <alvespaulo593@gmail.com>
2025-11-14 19:35:58 -0300

    REFACTOR - 'update card detail components'

--- js/renderICardsDetails.js
+++ js/renderICardsDetails.js
@@ -6,14 +6,16 @@ export function renderICardsDetails(container, data) {
     const card_details = document.createElement("card-details-component");
 
     const title = data.original_title || data.name || "Título indisponível";
-    const overview = data.overview;
+    const overview = data.overview || "Sinopse indisponível"
     const poster = data.poster_path
       ? `https://image.tmdb.org/t/p/w500${data.poster_path}`
       : "assets/no-image.png";
-    const genresOj = data.genres || [];
-    console.log(genresOj)
-    const genrersName = genresOj.map(element => element = element.name);
-    const seasons = data.seasons;
+    const genresObj = data.genres || [];
+    console.log(genresObj)
+    const genrersName = genresObj.map(element => element = element.name);
+    const seasonsObj = data.seasons || [];
+    const seasonsName = seasonsObj.map(element => element = element.name)
+    const episodes = seasonsObj.map(element => element = element.episode_count) || []
     const release = data.release_date || data.first_air_date || "—";
     
     const rawRate = data.vote_average ?? data.popularity;
@@ -21,10 +23,11 @@ export function renderICardsDetails(container, data) {
 
 
     card_details.setAttribute("title", title);
-    card_details.setAttribute("overview", overview)
+    card_details.setAttribute("overview", overview);
     card_details.setAttribute("poster", poster);
-    card_details.setAttribute("genres", genrersName)
-    card_details.setAttribute("seasons", seasons)
+    card_details.setAttribute("genres", genrersName);
+    card_details.setAttribute("seasons", seasonsName);
+    card_details.setAttribute("episodes", episodes);
     card_details.setAttribute("release", release);
     card_details.setAttribute("rate", rate);
 
