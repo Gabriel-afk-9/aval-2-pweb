417199dbb95bdc042b7c75c5e6a5b9b2f7733c94
Zidan-09 <samuel.profissional18@gmail.com>
2025-11-14 21:15:25 -0300

    FEAT - add background color and media type display to card component

--- components/card.js
+++ components/card.js
@@ -8,6 +8,7 @@ class Card extends HTMLElement {
     const poster = this.getAttribute("poster");
     const rate = this.getAttribute("rate");
     const release = this.getAttribute("release");
+    const bgColor = this.getAttribute("bgcolor");
 
     this.innerHTML = `
       <div class="card">
@@ -16,9 +17,15 @@ class Card extends HTMLElement {
           <h3 class="card-title">${title}</h3>
           <p class="card-release">Lançamento: ${release}</p>
           <p class="card-rate">Avaliação: ${rate}</p>
+          <div class="card-media">
+            <p class="media-type">${media_type}</p>
+          </div>
         </div>
       </div>
     `;
+
+    const media = this.querySelector(".card-media");
+    if (media) media.style.backgroundColor = bgColor;
   }
 }
 customElements.define("card-component", Card);
